2423af461c9d37c54445150f3a3302c2310027ef
lilybrownmk-creator <lily.brown.mk@gmail.com>
2026-07-01 00:43:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react' 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import React, { useEffect } from 'react' 
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { queryClient } from '@/lib/queryClient'
 import { LanguageProvider } from '@/lib/i18n'
+import ReactPixel from 'react-facebook-pixel' // Го додаваме пакетот за Pixel
 
 import Shop              from '@/pages/Shop'
 import Wishlist          from '@/pages/Wishlist'
@@ -15,14 +16,35 @@ import AdminGuard        from '@/components/AdminGuard'
 import OrderConfirmation from '@/pages/OrderConfirmation'
 import ProductDetail from '@/pages/ProductDetail'
 
+// Оваа компонента ги следи промените на линковите во прелистувачот (/wishlist, /orders...)
+function PixelTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Секој пат кога корисникот ќе смени страница, му јавуваме на Meta
+    ReactPixel.pageView();
+  }, [location]);
+
+  return null;
+}
+
 export default function App() {
+  // Го активираме Pixel-от со твојот точен број веднаш штом ќе се вчита сајтот
+  useEffect(() => {
+    ReactPixel.init('266319177839439'); // Твојот точен број е веќе тука
+    ReactPixel.pageView();
+  }, []);
+
   return (
     <LanguageProvider>
     <QueryClientProvider client={queryClient}>
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        {/* Го ставаме следачот внатре во Router */}
+        <PixelTracker />
+
         <Routes>
           {/* ── Storefront ── */}
-          <Route path="/"         element={<Shop />} />
+          <Route path="/"          element={<Shop />} />
           <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/bundle"   element={<BundleBuilder />} />
           <Route path="/orders"   element={<MyOrders />} />
